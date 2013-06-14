e5b8b09d4a75e904840306fcaa6efe1a5591d7ba
Suhail Shergill <suhailshergill@gmail.com>
2013-06-13 23:29:32 -0400

    bugfix: Fix fontconfig /etc/fonts default
    from http://lists.science.uu.nl/pipermail/nix-dev/2013-June/011303.html
    
    > The problem is that fontconfig has renamed thier --with-confdir
    > configuration option to --with-baseconfigdir.
    
    > Can you or someone else patch
    > pkgs/development/libraries/fontconfig/default.nix ?
    
    Fixes #620

--- pkgs/development/libraries/fontconfig/default.nix
+++ pkgs/development/libraries/fontconfig/default.nix
@@ -12,7 +12,7 @@ stdenv.mkDerivation rec {
 
   propagatedBuildInputs = [ expat ]; # !!! shouldn't be necessary, but otherwise pango breaks
 
-  configureFlags = "--with-confdir=/etc/fonts --with-cache-dir=/var/cache/fontconfig --disable-docs --with-default-fonts=";
+  configureFlags = "--with-baseconfigdir=/etc/fonts --with-cache-dir=/var/cache/fontconfig --disable-docs --with-default-fonts=";
 
   # We should find a better way to access the arch reliably.
   crossArch = stdenv.cross.arch or null;
